23e97ab3c553a791f05a1b2b240bb601491161b0
Marcos Soares <mvsoaresrj21@gmail.com>
2024-05-04 21:47:08 -0300

    Meu Primeiro Commit do Projeto

---
+++ meu-primeiro-projeto-java/src/OlaBemVindo.java
@@ -0,0 +1,5 @@
+public class OlaBemVindo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Bem-Vindos ao meu primeiro Projeto Java!");
+    }
+}
